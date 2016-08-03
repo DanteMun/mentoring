127604a865d928cb97c56cf3bd1bec47c5581d4c
Bae <bjh512512@gmail.com>
2016-08-03 10:14:36 +0900

    주혁_160803_소팅+상위 몇 명까지 커트

--- auction/auction_algo.py
+++ auction/auction_algo.py
@@ -2,8 +2,8 @@ from auction.models import MenToMen_Rel
 
 def auction_algo():
 	mentor_id = 1
+	mentee_num = 3
 	li = list(MenToMen_Rel.objects.filter(mentors=mentor_id).values())
-#li.sort(key=lambda li:li["mentees_id"]),key=lambda li:li["price"],reverse=True
 	li_sorted = sorted(sorted(li,key=lambda li:li["time"]),key=lambda li:li["price"],reverse=True)
-	
-	return li_sorted
+	li_ranked = li_sorted[:mentee_num]
+	return li_ranked
